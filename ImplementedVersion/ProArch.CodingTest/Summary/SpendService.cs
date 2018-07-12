014666f1224e7f11c07f76991300f8e389b705df
DESKTOP-PG56K6G\ipydev <ironpython2001@gmail.com>
2018-07-12 06:28:05 +0530

    modified

--- ImplementedVersion/ProArch.CodingTest/Summary/SpendService.cs
+++ ImplementedVersion/ProArch.CodingTest/Summary/SpendService.cs
@@ -21,10 +21,40 @@ namespace ProArch.CodingTest.Summary
                 //need to change with dependency injection
                 var invoiceRepository = new Invoices.InvoiceRepository();
                 var invoices= invoiceRepository.Get(supplier.Id);
-                //invoices.Sum()
+                //sum with group by year
+                var result = invoices.GroupBy(x => x.InvoiceDate.Year)
+                                .Select(g => new
+                                {
+                                    Year = g.Key,
+                                    TotalSpend = g.Sum(x=>x.Amount)
+                                }).ToList();
+                result.ForEach(x => spendSummary.Years.Add(
+                    new SpendDetail()
+                    {
+                        Year= x.Year,
+                        TotalSpend = x.TotalSpend
+                    }));
+                
             }
             else //External Service
             {
+                //need to change with dependency injection and repository pattern
+                var externalInvoices= ProArch.CodingTest.External.ExternalInvoiceService.GetInvoices(supplier.Id.ToString());
+                //sum with group by year
+                var result = externalInvoices.GroupBy(x => x.Year)
+                                .Select(g => new
+                                {
+                                    Year = g.Key,
+                                    TotalSpend = g.Sum(x => x.TotalAmount)
+                                }).ToList();
+
+                result.ForEach(x => spendSummary.Years.Add(
+                    new SpendDetail()
+                    {
+                        Year = x.Year,
+                        TotalSpend = x.TotalSpend
+                    }));
+
 
             }
             
